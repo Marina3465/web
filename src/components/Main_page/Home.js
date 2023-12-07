b8c232d1f5e1b6e49b8af574f4b21e9c2c582af4
Marina Kozhukhar <kozhyxarm12@gmail.com>
2023-12-07 19:48:49 +0300

    add footer and edit filters

--- src/components/Main_page/Home.js
+++ src/components/Main_page/Home.js
@@ -7,8 +7,8 @@ import './Home.css';
 
 function Home() {
     {
-        const num_cart = [1,2,3,4,5,6,7,8];
-        
+        const num_cart = [1, 2, 3, 4, 5, 6, 7, 8];
+
         const [showModal, setShowModal] = useState(false);
         return (
             <div className='conteiner-home'>
@@ -31,33 +31,47 @@ function Home() {
                     <img src={require('../../img/logo.png')} alt='Логотип сайта' />
                 </div>
                 <div className='carts'>
-                    {num_cart.map(()=>(
+                    {num_cart.map(() => (
                         <><div className='cart'>
-                        <div className='cart-photo'></div>
-                        <div className='cart-content'>
-                            <div className='cart-header'>
-                                <h3 className='cart-header-title'>Куриная запеканка</h3>
-                                <p className='cart-header-info'>1000 ккал</p>
+                            <div className='cart-photo'></div>
+                            <div className='cart-content'>
+                                <div className='cart-header'>
+                                    <h3 className='cart-header-title'>Куриная запеканка</h3>
+                                    <p className='cart-header-info'>1000 ккал</p>
+                                </div>
+                                <div className='cart-text'>
+                                    <p className='cart-data'>Фарш куриный, печень куриная, лук, чеснок, яйца, соль, перец чёрный молотый, приправа, хлеб белый, молоко, масло, сыр твёрдый</p>
+                                    <p className='cart-user'>Rambutan</p>
+                                    <p className='cart-time'>90 мин</p>
+                                    <p className='cart-tags'>курица, ужин, духовка, просто</p>
+                                </div>
                             </div>
-                            <div className='cart-text'>
-                                <p className='cart-data'>Фарш куриный, печень куриная, лук, чеснок, яйца, соль, перец чёрный молотый, приправа, хлеб белый, молоко, масло, сыр твёрдый</p>
-                                <p className='cart-user'>Rambutan</p>
-                                <p className='cart-time'>90 мин</p>
-                                <p className='cart-tags'>курица, ужин, духовка, просто</p>
-                            </div>
-                        </div>
-                    </div></>
-                        
+                        </div></>
+
                     ))}
-                    
+
 
                 </div>
 
+                <div className='load-cats'>Загрузить ещё</div>
 
 
-               {
-                showModal ? <Modal toggleModal={setShowModal} /> : null
-               }
+                {
+                    showModal ? <Modal toggleModal={setShowModal} /> : null
+                }
+
+                <div className='home-footer'>
+                    <ul className='list-link'>
+                        <li className='list-link-vk'><a href='#'></a></li>
+                        <li className='list-link-tg'><a href='#'></a></li>
+                        <li className='list-link-wt'><a href='#'></a></li>
+                    </ul>
+                    <h3 className='name-comp'>©️culinaryworld</h3>
+                    <ul className='home-footer-phone-email'>
+                        <li><a className='home-footer-phone' href='tel:+79000000000'>+7 (900) 000-00-00</a></li>
+                        <li><a className='home-footer-email' href='mailto:culinaryworld@mail.ru'>culinaryworld@mail.ru</a></li>
+                    </ul>
+                </div>
             </div>
         )
     }
--- src/components/Main_page/Modal.js
+++ src/components/Main_page/Modal.js
@@ -5,26 +5,27 @@ function Modal({ toggleModal }) {
   return (
     <div className='modal-container' >
       <div className={`modal-content ${toggleModal ? 'active' : ''}`}>
-        <h1>Фильтры</h1>
-        <button onClick={() => toggleModal(false)}>&times;</button>
-        <ul>
-          <li>
-            Способ приготовления
-            <label for="filter">Выберите опцию:</label>
-            <select id="filter">
-              
-              <option >Варка</option>
-              <option>Жарка</option>
-              <option>Тушение</option>
-            </select>
-            </li>
-          <li>Тип блюда</li>
-          <li>Вегетерианские блюда</li>
-          <li>Происхождение кухни</li>
-          <li>По времени приготовления</li>
-          <li>Острое</li>
-          <li>Низкокалорийное</li>
-          <li>Время дня</li>
+        <div className='filter-header'>
+          <h1>Фильтры</h1>
+          <button className='filter-close' onClick={() => toggleModal(false)}>&times;</button>
+        </div>
+        <ul className='menu-filter'>
+          <li className='item-filter'>
+            <span>Способ приготовления</span>
+            <ul className='list-filter-hidden'>
+              <li className='item-filter-hidden'><span>Варка</span></li>
+              <li className='item-filter-hidden'><span>Варка</span></li>
+              <li className='item-filter-hidden'><span>Варка</span></li>
+              <li className='item-filter-hidden'><span>Варка</span></li>
+            </ul>
+          </li>
+          <li className='item-filter'><span>Тип блюда</span></li>
+          <li className='item-filter'><span>Вегетерианские блюда</span></li>
+          <li className='item-filter'><span>Происхождение кухни</span></li>
+          <li className='item-filter'><span>По времени приготовления</span></li>
+          <li className='item-filter'><span>Острое</span></li>
+          <li className='item-filter'><span>Низкокалорийное</span></li>
+          <li className='item-filter'><span>Время дня</span></li>
         </ul>
 
       </div>
