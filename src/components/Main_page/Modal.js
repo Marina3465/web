9ecbd2a832d7121817cebe52d960648944c726c3
Marina Kozhukhar <kozhyxarm12@gmail.com>
2023-12-09 17:54:44 +0300

    edit filters

--- src/components/Main_page/Modal.js
+++ src/components/Main_page/Modal.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Modal.css';
 
 function Modal({ toggleModal }) {
+  const handleReload = () => {
+    window.location.reload();
+  };
   return (
     <div className='modal-container' >
       <div className={`modal-content ${toggleModal ? 'active' : ''}`}>
@@ -13,13 +16,20 @@ function Modal({ toggleModal }) {
           <li className='item-filter'>
             <span>Способ приготовления</span>
             <ul className='list-filter-hidden'>
-              <li className='item-filter-hidden'><span>Варка</span></li>
-              <li className='item-filter-hidden'><span>Варка</span></li>
-              <li className='item-filter-hidden'><span>Варка</span></li>
-              <li className='item-filter-hidden'><span>Варка</span></li>
+              <li className='item-filter-hidden'><input type='checkbox'/><label>Варка</label></li>
+              <li className='item-filter-hidden'><input type='checkbox'/><label>Варка</label></li>
+              <li className='item-filter-hidden'><input type='checkbox'/><label>Варка</label></li>
+              <li className='item-filter-hidden'><input type='checkbox'/><label>Варка</label></li>
             </ul>
           </li>
-          <li className='item-filter'><span>Тип блюда</span></li>
+          <li className='item-filter'><span>Тип блюда</span>
+            <ul className='list-filter-hidden'>
+            <li className='item-filter-hidden'><input type='checkbox'/><label>Варка</label></li>
+              <li className='item-filter-hidden'><input type='checkbox'/><label>Варка</label></li>
+              <li className='item-filter-hidden'><input type='checkbox'/><label>Варка</label></li>
+              <li className='item-filter-hidden'><input type='checkbox'/><label>Варка</label></li>
+            </ul>
+          </li>
           <li className='item-filter'><span>Вегетерианские блюда</span></li>
           <li className='item-filter'><span>Происхождение кухни</span></li>
           <li className='item-filter'><span>По времени приготовления</span></li>
@@ -27,6 +37,10 @@ function Modal({ toggleModal }) {
           <li className='item-filter'><span>Низкокалорийное</span></li>
           <li className='item-filter'><span>Время дня</span></li>
         </ul>
+        <div className='modal-buttons'>
+          <button className='modal-apply'>Применить</button>
+          <button className='modal-reset' onClick={handleReload}>Сбросить</button>
+        </div>
 
       </div>
     </div>
